fd2ce7e4382396c83f8b9763729fee822b6a2a60
trifolium.wang <trifolium.wang@foxmail.com>
2024-05-17 11:22:20 +0800

    :whale: 修改了建议的jre镜像

--- services/src/main/java/com/example/solon/template/common/aot/RuntimeNativeRegistrarImpl.java
+++ services/src/main/java/com/example/solon/template/common/aot/RuntimeNativeRegistrarImpl.java
@@ -5,6 +5,7 @@ import org.noear.solon.aot.RuntimeNativeMetadata;
 import org.noear.solon.aot.RuntimeNativeRegistrar;
 import org.noear.solon.aot.hint.MemberCategory;
 import org.noear.solon.core.AppContext;
+import org.noear.solon.core.util.ResourceUtil;
 
 /**
  * @title: native镜像尝鲜，不建议使用
@@ -17,5 +18,6 @@ public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
     public void register(AppContext context, RuntimeNativeMetadata metadata) {
 //        metadata.registerReflection("org.noear.solon.serialization.prop.JsonProps", MemberCategory.values());
 //        metadata.registerReflection("org.noear.solon.serialization.prop.JsonProps", MemberCategory.values());
+//        ResourceUtil.scanResources("classpath:a/**/*").forEach(metadata::registerResourceInclude);
     }
 }
